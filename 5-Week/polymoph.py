c06c358ce68e84b317ca7beed6fdf936039a7482
quol <kuolamol77@gmail.com>
2025-04-16 14:50:12 +0300

    changed the folder structure

---
+++ 5-Week/polymoph.py
@@ -0,0 +1,15 @@
+# cat class
+class Dog:
+    def speak(self):
+        return "Woof!"
+# cat class
+class Cat:
+    def speak(self):
+        return "Meow!"
+
+# Polymorphism in action
+for animal in [Dog(), Cat()]:
+    print(animal.speak())
+# Output:
+# Woof!
+# Meow!
--- README.md
+++ README.md
@@ -11,3 +11,4 @@ This is a Repository for keeping track of Python challenges that are done on a w
 |File Handling| Week four|April|
 |Object Oriented Prog|Week Five| April|
 |Python Libraries|Week Six| April|
+|Data Analysis|Week Seven|April|
